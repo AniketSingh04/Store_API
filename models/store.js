a59e4dcbcda4be3433cd1c9972d1f000ad72f207
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-08 00:10:32 +0530

    DB Model and Schema

---
+++ models/store.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const StoreSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required: [true,'must provide name'], //if the value to name is passed of any other type then error will occur
+        trim : true,
+        maxlength : [20, 'name cannot exceed 20 characters'],
+    },
+    completed: {
+        type : Boolean,
+        default: false,
+    }
+});
+
+module.exports = mongoose.model("Store", StoreSchema);
